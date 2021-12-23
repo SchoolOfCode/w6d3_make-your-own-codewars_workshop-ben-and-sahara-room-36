d0aec0f53d15150c5dff7166fd5f6a6ff818094f
Sahara Rai <sahararai@hotmail.com>
2021-12-23 11:32:21 +0000

    non-prime function kata

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 /* 
 👉 Write your kata here!*/
-/*Write a function that takes in a number as a parameter and it returns true if it is a prime number or false if it is not a prime number.
+/*
+No prime
+Write a function that takes in an integer as a parameter. If it is a prime number it should return the next non-prime number. If it is a positive non-prime number, it should return itself. Whereas, if it is a negative number, it should return zero.
 
 Prime numbers are special numbers, greater than 1, that have exactly two factors, themselves and 1.
 
 For example 
-1 ----> false
-2 ----> true
-4 ----> false
-7 ----> true
--2 ----> false
+1 ----> 1
+2 ----> 4
+4 ----> 4
+7 ----> 8
+-2 ----> 0
 
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
 
-function isPrime(number) {
-  // Write a function that takes in a number as a parameter and it returns true if it is a prime number or false if it is not a prime number.
+export function noPrime(number) {
+  //code here
+  
 }
+
+
--- main.test.js
+++ main.test.js
@@ -1,57 +1,60 @@
+import {noPrime} from "./main.js";
+
 //👉 Write your tests below here:
 
-/*export function isPrime(number) {
-          for(let i = 2; i < number; i++)
-            if(number % i === 0) return false;
-          return number > 1;
-        
-  }*/
-describe("is Prime kata", () => {
-  const number = [3, 5, 7];
 
-  // test("should return false if input is 1", function () {
-  //   const expected = false;
-  //   const actual = false;
-  //   expect(actual).toBe(expected);
-  // });
 
-  // it.each(letterScores)(`should score %s as %i`, (letter, score) => {
+describe("no Prime kata", () => {
+
+  //  it.each(letterScores)(`should score %s as %i`, (letter, score) => {
   //   expect(scrabbleScore(letter)).toBe(score);
   // });
-
-  test("should return true for input 6", function () {
-    function isPrime(number) {
-      for (let i = 2; i < number; i++) if (number % i === 0) return false;
-      return number > 1;
-      expect(number).toBe(true);
-    }
-
-    // const expected = true;
-    // const actual = isPrime(primeNumbers);
+ 
+  test("should return 1 with input 1", function () {
+    const expected = 1;
+    const actual = noPrime(1);
+    expect(actual).toBe(expected);
   });
 
-  // test("should return false if number is negative", function () {
-  //   function isNegative(number) {
-  //     if (number < 0) return false;
-  //   }
-  //   const expected = false;
-  //   const actual = isNegative(-6);
+  test("should return 4 with input 2", function(){
+    const expected = 4;
+    const actual = noPrime(2);
 
-  //   expect(actual).toBe(expected);
+    expect(actual).toBe(expected);
+    });
+
+    test("should return 8 with input 7", function () {
+      const expected = 8;
+      const actual = noPrime(7);
+      expect(actual).toBe(expected);
+    });
+
+    test("should return 98 with input 97", function () {
+      const expected = 98;
+      const actual = noPrime(97);
+      expect(actual).toBe(expected);
+    });
+
+    test("should return 10 with input 10", function () {
+      const expected = 10;
+      const actual = noPrime(10);
+      expect(actual).toBe(expected);
+    });
+
+    test("should return 4 with input 4", function () {
+      const expected = 4;
+      const actual = noPrime(4);
+      expect(actual).toBe(expected);
+    });
+
+  test("should return 0 if number is negative", function () {
+    const expected = 0;
+    const actual = noPrime(-6);
+
+    expect(actual).toBe(expected);
+  });
+
+  
+
 });
 
-// const primeNumbers = [
-//   [0, false],
-//   [1, false],
-//   [2, true],
-//   [3, true],
-//   [5, true],
-//   [7, true],
-//   [11, true],
-//   [19, true],
-//   [75, false],
-//   [-1, false],
-//   [9, false],
-//   [4, false],
-//   [20, false]
-//   ];
